96694dd6160292b2ad6751a4e4e7fc2857810b38
tka4u <tka4uk.vany@gmail.com>
2026-01-17 12:52:31 +0300

    Добавленa 8 практическая

---
+++ PZ_8/PZ_8_2.py
@@ -0,0 +1,8 @@
+"""
+
+"""
+avto = {'reno': ['logan','daster','sanderro'],
+        'lada': ['largus','vesta','granta'],
+        'porshe': [911, 'targa','carrera']}
+for k, v in avto.items():
+    print(k, v[1])
