e1976e38cb8412a9b0ad26104d13a9c268fafd00
tka4u <tka4uk.vany@gmail.com>
2026-05-02 12:23:18 +0300

    Исправленна

--- PZ_8/PZ_8_2.py
+++ PZ_8/PZ_8_2.py
@@ -7,4 +7,5 @@ avto = {'reno': ['logan','daster','sanderro'],
         'lada': ['largus','vesta','granta'],
         'porshe': [911, 'targa','carrera']}
 for k, v in avto.items():
-    print(k, v[1])
+    print(f"Вторая модель {k}:",  v[1])
+    print(f"Все модели {k}:", v)
