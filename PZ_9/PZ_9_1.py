8a1e378ea5b5fa6ff52832a8f0960a6484b6a26c
tka4u <tka4uk.vany@gmail.com>
2026-06-13 12:19:54 +0300

    Исправленна 9 практическая

--- PZ_9/PZ_9_1.py
+++ PZ_9/PZ_9_1.py
@@ -2,57 +2,48 @@
 Имеется список студентов группы, в котором все имена различны.
 Определить, есть ли в группе студент, который побывал в гостях у всех студентов.
 (Для каждого студента составить список из множества побывавших у него в гостях друзей,
-причем хозяина в этот список не включать).
+причем хозяина в этот список не включать). & | - ==
 """
-students = ["Иван","Артем","Миша","Даня","Гена","Маша","Катя"]
-visited ={
-    "Иван": {"Даня","Гена","Маша","Катя"},
-    "Артем": {"Иван","Даня","Гена","Маша","Катя"},
-    "Миша": {"Иван","Артем","Даня","Гена","Маша","Катя"},
-    "Даня": {"Иван","Артем","Миша"},
-    "Гена": {"Миша","Даня"},
-    "Маша": {"Катя"},
-    "Катя": {"Иван","Артем","Миша"}
-}
+students = {"Иван","Артем","Миша","Даня","Гена","Маша","Катя"}
+visited_i = {"Даня","Гена","Маша","Катя"}
+visited_A = {"Иван","Даня","Гена","Маша","Катя"}
+visited_M = {"Иван","Артем","Даня","Гена","Маша","Катя"}
+visited_D = {"Иван","Артем","Миша"}
+visited_G = {"Иван","Миша","Даня"}
+visited_MA = {"Иван","Катя"}
+visited_K = {"Иван","Артем","Миша"}
 
-students_copy = students
-count = 0
-cc = 0
-while count < 7:
-    students = set(students_copy)
+result = 0
 
-    if count == 0:
-        students.remove("Иван")
-        if students == visited["Иван"]:
-            cc += 1
-    elif count == 1:
-        students.remove("Артем")
-        if students == visited["Артем"]:
-            cc += 1
-    elif count == 2:
-        students.remove("Миша")
-        if students == visited["Миша"]:
-            cc += 1
-    elif count == 3:
-        students.remove("Даня")
-        if students == visited["Даня"]:
-            cc += 1
-    elif count == 4:
-        students.remove("Гена")
-        if students == visited["Гена"]:
-            cc += 1
-    elif count == 5:
-        students.remove("Маша")
-        if students == visited["Маша"]:
-            cc += 1
-    elif count == 6:
-        students.remove("Катя")
-        if students == visited["Катя"]:
-            cc += 1
-    count += 1
+if ("Миша" in visited_i and "Миша" in visited_A and "Миша" in visited_D and
+    "Миша" in visited_G and "Миша" in visited_MA and "Миша" in visited_K):
+    result += 1
 
+if ("Иван" in visited_A and "Иван" in visited_M and "Иван" in visited_D and
+    "Иван" in visited_G and "Иван" in visited_MA and "Иван" in visited_K):
+    result += 1
 
-if cc > 0:
-    print(f"В группе {cc} студентов были у всех")
+if ("Артем" in visited_i and "Артем" in visited_M and "Артем" in visited_D and
+    "Артем" in visited_G and "Артем" in visited_MA and "Артем" in visited_K):
+    result += 1
+
+if ("Даня" in visited_i and "Даня" in visited_A and "Даня" in visited_M and
+    "Даня" in visited_G and "Даня" in visited_MA and "Даня" in visited_K):
+    result += 1
+
+if ("Гена" in visited_i and "Гена" in visited_A and "Гена" in visited_M and
+    "Гена" in visited_D and "Гена" in visited_MA and "Гена" in visited_K):
+    result += 1
+
+if ("Маша" in visited_i and "Маша" in visited_A and "Маша" in visited_M and
+    "Маша" in visited_D and "Маша" in visited_G and "Маша" in visited_K):
+    result += 1
+
+if ("Катя" in visited_i and "Катя" in visited_A and "Катя" in visited_M and
+    "Катя" in visited_D and "Катя" in visited_G and "Катя" in visited_MA):
+    result += 1
+
+if result > 0:
+    print(f"В группе {result} студентов были у всех")
 else:
     print("В группе нет студентов которые были у всех")
